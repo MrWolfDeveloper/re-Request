27e807581419cb6f7683ca0f30ea2e8d739c5967
Mohammad Pourshabanan <mohammadpourshabanan@gmail.com>
2021-11-18 14:14:39 +0330

    Save data

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
-const proxyCheck = require('./lib/proxyCheck').proxyCheck;
+const saveData = require('./lib/saveData').saveData;
 
-console.log(proxyCheck.check());
+const databaseQuery = require('./lib/database/databaseQuery').database;
+
+databaseQuery.connectDatabase(
+	'mongodb+srv://MrWolf:Aa106677889@exbdatabse.xnwy0.mongodb.net/exbdatabse?retryWrites=true&w=majority',
+	(error, connection) => {
+		if (error) throw new Error(error);
+
+		saveData.save(
+			{
+				requestUrl: 'https://google.com/test',
+				status: 'pending'
+			},
+			{
+				saveType: 'mongodb'
+			},
+			(errorSaveData, returnData) => {
+				if (errorSaveData) throw new Error(errorSaveData);
+
+				console.log(returnData);
+			}
+		);
+	}
+);
